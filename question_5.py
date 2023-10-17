0fcd00901705beb6c275f31ed78c5fd9c2f8fdea
ZelDerMon <loced63@gmail.com>
2023-10-17 14:10:24 +0300

    question 5 complited

---
+++ question_5.py
@@ -0,0 +1,15 @@
+num_v = int(input('Введите значение выручки - '))
+num_i = int(input('Введите значение издержек - '))
+while True:
+   if num_v >= num_i:
+    pr = num_v - num_i
+    print('Прибыль компании - ', pr)
+    ren = pr >= num_v
+    print('Рентабельность компании - ', ren)
+    st = int(input('Введите общее количество сотрудников - '))
+    st_pr = pr/st
+    print('Прибыль на одного сотрудника - ',st_pr)
+    break
+   if num_v <= num_i:
+    ub = print('Убыток компании - ',num_v - num_i )
+    break
