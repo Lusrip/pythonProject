7d179269f5fa73e00c7c42a9c88f0e9c72704554
ZelDerMon <loced63@gmail.com>
2023-10-17 10:44:23 +0300

    new update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='question 1',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='Duffledest',
+    author_email='',
+    description=''
+)
